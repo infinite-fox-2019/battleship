564c724262b20e3199104bd9901490c12765b8df
edwinsatya <edwinsatyayudistira@gmail.com>
2019-09-05 21:04:21 +0700

    tambah board

--- battleship.js
+++ battleship.js
@@ -1,1 +1,31 @@
 // Your code here
+function printBoard () {
+    let alphabet = ' ABCDEFGHIJ';
+    let board = [];
+    for (let i = 0; i < 11; i++) {
+        let tempBoard = [];
+        for (let j = 0; j < 11; j++) {
+            if (i === 0 && j !== i) {
+                tempBoard.push(String(j))
+            } else if (j === 0) {
+                tempBoard.push(alphabet[i]);
+            } else {
+                tempBoard.push(' ');
+            }
+        }
+        board.push(tempBoard);
+    }
+    return board;
+};
+
+function battleship () {
+    let tableShip = {
+        'Aircreaft carrier' : 5,
+        'Battleship' : 4,
+        'Cruiser' : 3,
+        'Destroyer' : 2
+    };
+};
+
+let arrayParameter = process.argv.slice(2);
+console.log(printBoard(arrayParameter[0], arrayParameter[1]));
